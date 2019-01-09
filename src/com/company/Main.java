6cd0c8e77d765abcb878134723d0ba077eb599f7
DSkilton <the_skiltons@hotmail.com>
2019-01-09 20:20:24 +0000

    Takes an Int Array and reverses

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,10 @@
 package com.company;
 
+import com.sun.deploy.util.ArrayUtil;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 //first input will be length of array and second input will be array which is to be reversed and display with a space
 //between numbers, for example:
@@ -16,31 +19,24 @@ public class Main {
     public static void main(String[] args)throws java.lang.Exception {
         //use the following code to fetch input from console
         String line;
+        int[] list;
+        String[] stNew;
+        stNew = line.split("");
+        list = new int[stNew.length];
 
         BufferedReader inp = new BufferedReader (new InputStreamReader(System.in));
         line=inp.readLine();
 
-        int[] list;
-        String[] stNew;
-
-        stNew = line.split(" ");
-        System.out.println(stNew);
-
-        list = new int[stNew.length];
-        System.out.println(list);
-
-        for (int i = 0; i < stNew.length; i++ ){
+        for (int i = 0; i < list.length; i++ ) {
             list[i] = Integer.parseInt(stNew[i]);
-
-            for (int j = 0; j < list.length +1 / 2; j++) {
-                int temp = list[i];
-                list[i] = list[list.length - j - 1];
-                list[list.length - j - 1] = temp;
-            }
-
         }
 
-        System.out.println(line);
+        for (int i = 0; i < list.length / 2; i++) {
+            int temp = list[i];
+            list[i] = list[list.length - 1 - i];
+            list[list.length - 1 - i] = temp;
+        }
 
+        System.out.println(Arrays.toString(list));
     }
 }
