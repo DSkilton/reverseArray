cea572ce06ff3bd79958e57432cf828a5e5e2947
Duncan Skilton <32618413+DSkilton@users.noreply.github.com>
2019-01-15 20:08:27 +0000

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,42 +1,43 @@
-package com.company;
-
-import com.sun.deploy.util.ArrayUtil;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-//first input will be length of array and second input will be array which is to be reversed and display with a space
-//between numbers, for example:
-//5
-//12345
-//
-//would be
-//54321
-
 public class Main {
 
-    public static void main(String[] args)throws java.lang.Exception {
-        //use the following code to fetch input from console
+    public static void main(String[] args) throws java.lang.Exception {
         String line;
-        int[] list;
-        String[] stNew;
-        stNew = line.split("");
-        list = new int[stNew.length];
+        String stNew;
 
         BufferedReader inp = new BufferedReader (new InputStreamReader(System.in));
-        line=inp.readLine();
 
-        for (int i = 0; i < list.length; i++ ) {
-            list[i] = Integer.parseInt(stNew[i]);
+        //first input which sets array length
+        line = inp.readLine();
+        stNew = inp.readLine();
+
+        //first input as array size and create array
+        int iArrayLength = Integer.parseInt(line);
+        int[] list = new int[iArrayLength];
+
+        //second input to populate array
+        String[] stArray = stNew.split(" ");
+
+        for (int i = 0; i < iArrayLength; i++ ){
+            list[i] = Integer.parseInt(stArray[i]);
         }
 
-        for (int i = 0; i < list.length / 2; i++) {
-            int temp = list[i];
-            list[i] = list[list.length - 1 - i];
-            list[list.length - 1 - i] = temp;
-        }
+        while (stArray.length != iArrayLength) {
+            System.out.println("Please input " + iArrayLength + " integers");
 
+            for (int i = 0; i < iArrayLength; i++) {
+                list[i] = Integer.parseInt(stArray[i]);
+            }
+
+            for (int i = 0; i < list.length / 2; i++) {
+                int temp = list[i];
+                list[i] = list[list.length - 1 - i];
+                list[list.length - 1 - i] = temp;
+            }
+        }
         System.out.println(Arrays.toString(list));
     }
 }
